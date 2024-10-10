74a5b0a75db2d9ec8588ff1b40888bbf07a27cd5
Tiago Romero <tiagoromerogarcia@gmail.com>
2024-10-10 14:21:24 -0700

    Initial site setup

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,5 +1,5 @@
 import { z, defineCollection } from "astro:content";
-const blogSchema = z.object({
+const projectSchema = z.object({
     title: z.string(),
     description: z.string(),
     pubDate: z.coerce.date(),
@@ -11,26 +11,10 @@ const blogSchema = z.object({
     }).optional(),
 });
 
-const storeSchema = z.object({
-    title: z.string(),
-    description: z.string(),
-    custom_link_label: z.string(),
-    custom_link: z.string().optional(),
-    updatedDate: z.coerce.date(),
-    pricing: z.string().optional(),
-    oldPricing: z.string().optional(),
-    badge: z.string().optional(),
-    checkoutUrl: z.string().optional(),
-    heroImage: z.string().optional(),
-});
+export type ProjectSchema = z.infer<typeof projectSchema>;
 
-export type BlogSchema = z.infer<typeof blogSchema>;
-export type StoreSchema = z.infer<typeof storeSchema>;
-
-const blogCollection = defineCollection({ schema: blogSchema });
-const storeCollection = defineCollection({ schema: storeSchema });
+const projectsCollection = defineCollection({ schema: projectSchema });
 
 export const collections = {
-    'blog': blogCollection,
-    'store': storeCollection
+    'projects': projectsCollection,
 }
