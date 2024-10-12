120051a81e47835d3978bc58387f5e8d3061b7bd
Tiago Romero <tiagoromerogarcia@gmail.com>
2024-10-11 20:07:37 -0700

    form touch-ups

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,14 +1,14 @@
 import { z, defineCollection } from "astro:content";
 const projectSchema = z.object({
-    title: z.string(),
-    description: z.string(),
-    pubDate: z.coerce.date(),
-    updatedDate: z.string().optional(),
-    heroImage: z.string().optional(),
-    badge: z.string().optional(),
-    tags: z.array(z.string()).refine(items => new Set(items).size === items.length, {
-        message: 'tags must be unique',
-    }).optional(),
+  title: z.string(),
+  description: z.string(),
+  pubDate: z.coerce.date(),
+  updatedDate: z.string().optional(),
+  heroImage: z.string().optional(),
+  badge: z.string().optional(),
+  tags: z.array(z.string()).refine(items => new Set(items).size === items.length, {
+      message: 'tags must be unique',
+  }).optional(),
 });
 
 export type ProjectSchema = z.infer<typeof projectSchema>;
@@ -16,5 +16,5 @@ export type ProjectSchema = z.infer<typeof projectSchema>;
 const projectsCollection = defineCollection({ schema: projectSchema });
 
 export const collections = {
-    'projects': projectsCollection,
+  'projects': projectsCollection,
 }
