2336865ce291d072d089b75621f09ed0affd11ef
Tiago Romero <tiagoromerogarcia@gmail.com>
2024-10-11 22:04:49 -0700

    update domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://personal-site-1ay.pages.dev',
+  site: 'https://tiagoromero.me',
   integrations: [mdx(), sitemap(), tailwind()]
 });
